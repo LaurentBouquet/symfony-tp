1eac964651788d725e051ecdaca0993d3d736837
LaurentBouquet <laurent.bouquet@joliciel.fr>
2020-09-30 17:19:50 +0200

    first commit

---
+++ src/Controller/TableController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\TableChoiceType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/table", name="table")
+ */
+class TableController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('table/index.html.twig', [
+            'controller_name' => 'TableController',
+        ]);
+    }
+
+    /**
+     * @Route("/print", name="print")
+     */
+    public function print(Request $request)
+    {
+        // dump($request);
+        $n = $request->get('n');
+        // dump($n);
+
+        return $this->render('table/print.html.twig', [
+            'n' => $n,
+        ]);
+    }
+
+    /**
+     * @Route("/select")
+     */
+    public function select(Request $request)
+    {
+        $form = $this->createForm(TableChoiceType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            $ret['n'] = $data['table_number'];
+            $response = $this->redirectToRoute('tableprint', $ret);
+        } else {
+            $response = $this->render('table/select.html.twig', [
+                'formulaire' => $form->createView(),
+            ]);
+        }
+
+        return $response;
+        
+
+
+
+    }
+
+}
